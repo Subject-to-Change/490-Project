9ecc384a83ae5c211c9fd3c6aa98a7bd9c6d7e81
Chyberia <cmyonker@uncg.edu>
2023-04-15 11:06:55 -0400

    Some script updates

--- Assets/Scripts/LevelScripts/PuzzleActivator.cs
+++ Assets/Scripts/LevelScripts/PuzzleActivator.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class PuzzleActivator : MonoBehaviour
 {
 
-    public PuzzleControllerScript controller;
-    int piece = 1;
+    public GameObject controller;
+    public int piece = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void setPiece()
+    {
+
+        FindObjectOfType<PuzzleControllerScript>().addOrder(piece);
+        Debug.Log("setPiece reached");
     }
 
     
--- Assets/Scripts/LevelScripts/PuzzleControllerScript.cs
+++ Assets/Scripts/LevelScripts/PuzzleControllerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
 public class PuzzleControllerScript : MonoBehaviour
 {
@@ -9,40 +10,43 @@ public class PuzzleControllerScript : MonoBehaviour
     int[] order;
     int[] solution;
     int i;
+    bool solved;
+
     // Start is called before the first frame update
     void Start()
     {
         i = 0;
-        order = new int[3];
-        solution = new int[3];
+        order = new int[4];
+        solution = new int[4];
         solution[0] = 1;
-        solution[1] = 2;
-        solution[2] = 3;
+        solution[1] = 3;
+        solution[2] = 4;
+        solution[3] = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (solution.Equals(order))
-       {
 
-            //call event to open door
+        if (solution.SequenceEqual(order))
+        {
+
             Debug.Log("Solution reached");
-        }
 
-         else if (order[2] != 0)
-        {   
+        }
+         else if (order[3] != 0)
+        {
 
             Array.Clear(order, 0, order.Length);
             i = 0;
+            Debug.Log("Clear reached");
         }
 
     }
 
     public void addOrder(int piece)
     {
-
+        Debug.Log("addOrder reached");
         order[i] = piece;
         i++;
     }
